16ab213d98d32a824dd2d9ff5132ef37ef56187e
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2015-08-09 12:51:25 -0700

    Update helper.py

--- dashboard/shared/helper.py
+++ dashboard/shared/helper.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2015 Jeffrey Meyers
+# This program is released under the "MIT License".
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
+
 import csv, os
 
 from sqlalchemy import func, desc, distinct, cast, Integer
