f98431c4a53a5a9fe3535b87d3e327962c8e0fbe
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2015-08-09 12:52:56 -0700

    Update models.py

--- dashboard/mod_onoff/models.py
+++ dashboard/mod_onoff/models.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2015 Jeffrey Meyers
+# This program is released under the "MIT License".
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
+
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Numeric
