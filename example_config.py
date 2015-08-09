6324084ed67ce7a95dd7e88904b137acbe123e03
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2015-08-09 12:43:50 -0700

    Update example_config.py

--- example_config.py
+++ example_config.py
@@ -1,2 +1,8 @@
+# Copyright (C) 2015 Jeffrey Meyers
+# This program is released under the "MIT License".
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
 DB_CONFIG = "postgresql://user:password@host/database"
 ROOT_DIR = "/path/to/project"
+
