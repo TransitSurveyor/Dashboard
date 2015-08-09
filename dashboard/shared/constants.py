d006fc48a429b0a33f66ad427b73b6b55c9d49c8
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2015-08-09 12:51:15 -0700

    Update constants.py

--- dashboard/shared/constants.py
+++ dashboard/shared/constants.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2015 Jeffrey Meyers
+# This program is released under the "MIT License".
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
 """ Scans
 """
 ID = 'id'
